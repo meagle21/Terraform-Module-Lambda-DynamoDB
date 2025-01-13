9ef8207ad4cdf2e94a39cba3a893cb43fbed5a61
meagle21 <emanuel.eagle@gmail.com>
2025-01-13 15:35:56 -0500

    update

--- input_variables.tf
+++ input_variables.tf
@@ -1,7 +1,6 @@
 variable lambda_function_name {
     description = "Name of the lambda function"
     type = string
-    default = ""
     validation {
         condition = length(var.lambda_function_name) == 0
         error_message = "Lambda Function Name is required!"
@@ -11,7 +10,6 @@ variable lambda_function_name {
 variable sportsbook_website_url {
     description = "URL of the sportsbook"
     type = string
-    default = ""
     validation {
         condition = length(var.sportsbook_website_url) == 0
         error_message = "Sportsbook Website URL is required!"
@@ -21,7 +19,6 @@ variable sportsbook_website_url {
 variable dynambodb_table_name {
     description = "Name of DynamoDB table"
     type = string
-    default = ""
     validation {
         condition = length(var.dynambodb_table_name) == 0
         error_message = "DynamoDB Table Name is required!"
@@ -31,18 +28,15 @@ variable dynambodb_table_name {
 variable sportsbook {
     description = "Name of sportsbooks that the lambda/dynamodb are an API for"
     type = string
-    default = ""
 }
 
 variable sport {
     description = "Sport that the data is in relation to"
     type = string
-    default = ""
 }
 
 variable team_odds_bool {
     description = "Boolean, defines whether or not the odds are team based odds or are player props"
     type = bool
-    default = true
 }
 
