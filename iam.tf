ce2d610ae3351e889582bb042663262c394cb699
meagle21 <emanuel.eagle@gmail.com>
2025-01-13 15:47:30 -0500

    update

--- iam.tf
+++ iam.tf
@@ -15,3 +15,28 @@ resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
+resource "aws_iam_policy" "lambda_dynamodb_write_policy" {
+  name        = "lambda-dynamodb-write-policy"
+  description = "Policy to allow Lambda to write data to DynamoDB"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:BatchWriteItem"
+        ]
+        Effect   = "Allow"
+        Resource = module.dynamodb-table.dynamodb_table_arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attachment" {
+  policy_arn = aws_iam_policy.lambda_dynamodb_write_policy.arn
+  role       = aws_iam_role.iam_for_lambda.name
+}
