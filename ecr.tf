0b1ba8b9692ea1e48204d8d42a802f1581f5105d
meagle21 <emanuel.eagle@gmail.com>
2025-01-13 17:44:20 -0500

    update

---
+++ ecr.tf
@@ -0,0 +1,7 @@
+resource "aws_ecr_repository" "repo" {
+  name                 = concat("repo-for-", var.lambda_function_name)
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
